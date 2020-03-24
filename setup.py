0e0806f313309225af3c284f5bf3f913212e8471
bigboi300 <vasus714@yandex.ru>
2020-03-24 19:53:58 +0300

    add setup.py

--- setup.py
+++ setup.py
@@ -1,34 +1,25 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-from answer_generator import generate_answer
-
-
-def start(update, context):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
-
-
-def help(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Ask me something!')
-
-
-def generate(update, context):
-    """Echo the user message."""
-    update.message.reply_text(generate_answer(update.message.text))
-
-
-def main():
-    """Start bot"""
-    updater = Updater("paste ur bot token", use_context=True)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(MessageHandler(Filters.text, generate))
-
-    updater.start_polling()
-    updater.idle()
-
-
-if __name__ == '__main__':
-    main()
+setup(
+    name='bigboisbot',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    py_modules=[
+        'answer_generator',
+        'config',
+        'dataset',
+        'main',
+        ],
+    packages=[
+        'scipy',
+        'numpy',
+        'setuppy-generator',
+        'python-telegram-bot',
+        'scikit-learn',
+        'python-telegram-bot'
+        'nltk',
+        ],
+)
